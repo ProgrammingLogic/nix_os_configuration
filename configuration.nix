72368e2d5ea6e8ec6657c10cd978bf76031fc480
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2024-03-18 18:40:57 -0500

    Add configuration.nix

---
+++ configuration.nix
@@ -0,0 +1,282 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+let
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";	
+in
+{
+  imports = [ 
+      # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+
+      # Home Manager
+      (import "${home-manager}/nixos")
+  ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "jstiverson-dellxps_laptop"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Chicago";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = false;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.jstiverson = {
+    isNormalUser = true;
+    description = "Jonathyn Stiverson";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+
+    ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+	# Audio / video
+	vlc 
+
+	# Developer Tools
+	neovim	
+	git
+	nodejs_21
+	python3
+	vscode
+
+	# Internet
+	firefox
+
+	# Productivity
+	libreoffice
+	obsidian
+
+	# Communication
+	discord
+	signal-desktop
+
+	# Privacy & Security
+	protonvpn-gui
+	protonvpn-cli
+	bitwarden
+
+	# Entertainment
+	spotify
+	steam
+
+	# System
+	syncthing
+	syncthing-tray
+	tmux
+	wget
+	lsof
+	lshw
+	htop
+	fd
+
+  ];
+
+  # Exclude these gnome packages from the default install
+  environment.gnome.excludePackages = (with pkgs; [
+    gnome-photos
+    gnome-tour
+  ]) ++ (with pkgs.gnome; [
+    cheese # webcam tool
+    gnome-music
+    gedit # text editor
+    epiphany # web browser
+    geary # email reader
+    evince # document viewer
+    gnome-characters
+    totem # video player
+    tali # poker game
+    iagno # go game
+    hitori # sudoku game
+    atomix # puzzle game
+  ]);
+
+
+  # Gnome Configuration
+  home-manager.users.jstiverson = {
+    home.stateVersion = "23.11";
+
+    dconf = {
+      enable = true;
+
+      # Dark Mode
+      settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+    };
+
+    # Firefox Configuration
+    programs.firefox = {
+      enable = true;
+
+      # https://duckduckgo.com/?q=%22nixos%22+%22firefox%22&ia=web
+      # https://discourse.nixos.org/t/best-way-to-configure-firefox-extensions/32600
+      # https://github.com/TLATER/dotfiles/blob/b39af91fbd13d338559a05d69f56c5a97f8c905d/home-config/config/graphical-applications/firefox.nix
+      # https://github.com/notusknot/dotfiles-nix/blob/main/modules/firefox/default.nix
+
+      profiles.jstiverson = {
+        settings = {
+          # Disable telemetry
+          "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+          "browser.ping-centre.telemetry" = false;
+          "browser.tabs.crashReporting.sendReport" = false;
+          "devtools.onboarding.telemetry.logged" = false;
+          "toolkit.telemetry.enabled" = false;
+          "toolkit.telemetry.unified" = false;
+          "toolkit.telemetry.server" = "";
+
+          # Disable Pocket
+          "browser.newtabpage.activity-stream.feeds.discoverystreamfeed" = false;
+          "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
+          "browser.newtabpage.activity-stream.section.highlights.includePocket" = false;
+          "browser.newtabpage.activity-stream.showSponsored" = false;
+          "extensions.pocket.enabled" = false;
+
+          # Disable JS in PDFs
+          "pdfjs.enableScripting" = false;
+	};
+      };
+
+
+      policies = {
+        ExtensionsSettings = {
+          # Privacy Badger:
+          "jid1-MnnxcxisBPnSXQ@jetpack" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
+            installation_mode = "force_installed";
+          };
+        };
+      };
+    };
+
+    programs.git = {
+      enable = true;
+      userName = "Jonathyn Stiverson";
+      userEmail = "jlstiverson2002@protonmail.com";
+    };
+
+    programs.neovim = {
+      enable = true;
+      extraConfig = ''
+        set number
+	set expandtab
+      '';
+    };
+  };
+
+  
+
+
+  # Enable using xdg.mine to set default apps
+  xdg.mime.enable = true;
+
+  # Set default applications
+  xdg.mime.defaultApplications = {
+    # Web Browser
+    "text/html" = "org.qutebrowser.qutebrowser.desktop";
+    "x-scheme-handler/http" = "org.qutebrowser.qutebrowser.desktop";
+    "x-scheme-handler/https" = "org.qutebrowser.qutebrowser.desktop";
+    "x-scheme-handler/about" = "org.qutebrowser.qutebrowser.desktop";
+    "x-scheme-handler/unknown" = "org.qutebrowser.qutebrowser.desktop";
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  networking.firewall.allowedTCPPorts = [
+    22
+  ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+  nixpkgs.config.permittedInsecurePackages = [
+    "electron-25.9.0"
+  ];
+}
