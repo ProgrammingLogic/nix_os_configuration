b6a92232fd22e9a10ec796ee419c69efbe892619
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2024-03-19 08:35:14 -0500

    Add missng semicolon

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -66,7 +66,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "jstiverson-workstation" # Define your hostname.
+  networking.hostName = "jstiverson-workstation"; # Define your hostname.
 
   # Enable networking
   networking.networkmanager.enable = true;
