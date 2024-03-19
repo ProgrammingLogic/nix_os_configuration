f4c1900ca5bb234f1e1a9508c21cf47be7ec98bf
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2024-03-19 08:34:52 -0500

    Update setup.sh to include right device name

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/run/current-system/sw/bin/bash
 
 NIX_CONFIG="experimental-features = nix-command flakes"
-sudo nixos-rebuild switch --flake ".#$(hostname)"
+sudo nixos-rebuild switch --flake ".#jstiverson-workstation"
