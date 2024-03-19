5bbdf8bcc01683e553121924652d2baf992818a3
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2024-03-19 11:57:30 -0500

    Add base flake.nix

--- flake.nix
+++ flake.nix
@@ -1,15 +1,21 @@
 {
-  description = "A very basic flake";
+  description = "My personal Flake";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-
+  outputs = { self, nixpkgs }@attrs: {
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
     packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
 
+    nixosConfigurations.jstiverson-laptop = nixpkgs.lib.nixosSystem {
+     system = "x86_64-linux";
+     specialArgs = attrs;
+     modules = [
+      ./nixos/configuratuion.nix
+     ];
+    };
   };
 }
