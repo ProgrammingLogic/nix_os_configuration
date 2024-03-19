5eac6ab98efc75af37985188db8a28ba5d55eff2
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2024-03-18 22:52:12 -0500

    Configure SSH and initial user account

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -52,17 +52,12 @@
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
+
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
@@ -71,8 +66,15 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.jstiverson = {
     isNormalUser = true;
+    initialPassword = "changeme22!";
     description = "Jonathyn Stiverson";
     extraGroups = [ "networkmanager" "wheel" ];
+
+
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDyA94BA6rCIb8Zz2/3EbvdYIgG9gS98baoKA3E0s1a5 jstiverson@nixos"
+    ];
+
     packages = with pkgs; [
         # Internet
         pkgs.firefox
@@ -146,16 +148,19 @@
 
   # List services that you want to enable:
 
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  # Configure OpenSSH daemon
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "no";
+      PasswordAuthentication = false;
+    };
+  };
 
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [
     22
   ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
