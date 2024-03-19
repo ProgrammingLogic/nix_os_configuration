b74c361271efc1de1e9f021916c87a6d7d789502
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2024-03-19 11:30:14 -0500

    Remove cd ../ from scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,4 @@
 #!/run/current-system/sw/bin/bash
 
 NIX_CONFIG="experimental-features = nix-command flakes"
-
-cd ../
 sudo nixos-rebuild switch --flake ".#jstiverson-workstation"
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,4 @@
 #!/run/current-system/sw/bin/bash
 
-cd ../
-
 sudo cp nix/* /etc/nixos/
 sudo nixos-rebuild test
