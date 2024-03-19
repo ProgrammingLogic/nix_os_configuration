59d82b10ca69a9873fb36e0411eec9b33d5d08d4
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2024-03-19 10:51:08 -0500

    Move scripts to scripts/

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,6 @@
 #!/run/current-system/sw/bin/bash
 
 NIX_CONFIG="experimental-features = nix-command flakes"
+
+cd ../
 sudo nixos-rebuild switch --flake ".#jstiverson-workstation"
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,6 @@
 #!/run/current-system/sw/bin/bash
 
+cd ../
+
 sudo cp nix/* /etc/nixos/
 sudo nixos-rebuild test
