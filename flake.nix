528b53141967357fb3935d38105473500f5a1aa1
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2024-03-20 14:47:42 -0500

    Comment out home-manager usage, to ensure configuration.nix works before implementing more advanced configurations

--- flake.nix
+++ flake.nix
@@ -4,15 +4,15 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-    home-manager.url = "github:nix-community/home-manager/release-23.05";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    # home-manager.url = "github:nix-community/home-manager/release-23.05";
+    # home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
   
 
   outputs = { 
     self, 
     nixpkgs, 
-    home-manager, 
+    #  home-manager, 
     ... 
   } @ inputs: 
   let inherit (self) outputs; in {
@@ -22,15 +22,15 @@
           {inherit inputs outputs};
         };
         
-        # Home-manager requires 'pkgs' instance
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; 
+        # # Home-manager requires 'pkgs' instance
+        # pkgs = nixpkgs.legacyPackages.x86_64-linux; 
 
         modules = [
           # System configuration
           ./nixos/configuration.nix
 
-          # User configuration
-          ./home-manager/home.nix
+          # # User configuration
+          # ./home-manager/home.nix
         ];
       };
     };
