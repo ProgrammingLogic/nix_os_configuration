5643f777a03887ddd5eb29bfba484e5f50e47c0c
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2024-03-22 11:36:36 -0500

    Enable home-manager package

--- flake.nix
+++ flake.nix
@@ -4,15 +4,15 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-    # home-manager.url = "github:nix-community/home-manager/release-23.05";
-    # home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
   
 
   outputs = { 
     self, 
     nixpkgs, 
-    #  home-manager, 
+    home-manager, 
     ... 
   } @ inputs: 
   let inherit (self) outputs; in {
@@ -29,8 +29,8 @@
           # System configuration
           ./nixos/configuration.nix
 
-          # # User configuration
-          # ./home-manager/home.nix
+          # User configuration
+          ./home-manager/home.nix
         ];
       };
 
@@ -39,8 +39,8 @@
           inherit inputs outputs;
         };
         
-        # # Home-manager requires 'pkgs' instance
-        # pkgs = nixpkgs.legacyPackages.x86_64-linux; 
+        # Home-manager requires 'pkgs' instance
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; 
 
         modules = [
           # Hardware Configuration
@@ -49,8 +49,8 @@
           # System configuration
           ./nixos/configuration.nix
 
-          # # User configuration
-          # ./home-manager/home.nix
+          # User configuration
+          ./home-manager/home.nix
         ];
       };
     };
