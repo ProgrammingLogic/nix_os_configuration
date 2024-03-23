84b3c7e35ee07619cb107d157db0ef9a16f40eab
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2024-03-22 21:59:01 -0500

    Make home.nix a module

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,99 +1,85 @@
 # This is your home-manager configuration file
 # Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
 { inputs, lib, config, pkgs, ... }:
+let 
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+in
 {
-  # You can import other home-manager modules here
   imports = [
-    # If you want to use home-manager modules from other flakes (such as nix-colors):
-    # inputs.nix-colors.homeManagerModule
-
-    # You can also split up your configuration and import pieces of it here:
-    # ./nvim.nix
+    (import "${home-manager}/nixos")
   ];
 
   nixpkgs = {
-    # You can add overlays here
-    overlays = [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
+    overlays = [];
 
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
-    ];
-    # Configure your nixpkgs instance
+    # Might be able to these, not sure.
     config = {
-      # Disable if you don't want unfree packages
       allowUnfree = true;
-      # Workaround for https://github.com/nix-community/home-manager/issues/2942
       allowUnfreePredicate = _: true;
-    };
-  };
-
-  # TODO: Set your username
-  home = {
-    username = "jstiverson";
-    homeDirectory = "/home/jstiverson";
-  };
-
-  # Add stuff for your user as you see fit:
-  # programs.neovim.enable = true;
-  home.packages = with pkgs; [
-    # Internet
-    firefox
-
-    # Development
-    git
-    python3
-    vscode
-
-    # Productivity
-    libreoffice
-    obsidian
-
-    # Communication
-    discord
-    signal-desktop
-
-    # Privacy & Security
-    bitwarden
-
-    # Entertainment
-    spotify
-    steam
-
-    # System
-    syncthing
-    syncthing-tray
-
-    # Audio / video
-    vlc 
-  ];
-
-  # Enable home-manager
-  programs.home-manager.enable = true;
-
-
-  programs.git = {
-    enable = true;
-    lfs.enable = true;
-
-    userName = "Jonathyn Stiverson";
-    userEmail = "jlstiverson2002@protonmail.com";
-
-    aliases = {
-        gc = "commit";
-        gp = "push";
-        ga = "add";
     };
   };
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
 
-  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  home.stateVersion = "23.05";
+  home-manager.users.jstiverson  = {
+    programs.home-manager.enable = true;
+
+    home = {
+      # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+      stateVersion = "23.05";
+
+      username  = "jstiverson";
+      homeDirectory = "/home/jstiverson";
+
+      packages = [
+        # Internet
+        firefox
+
+        # Development
+        git
+        python3
+        vscode
+
+        # Productivity
+        libreoffice
+        obsidian
+
+        # Communication
+        discord
+        signal-desktop
+
+        # Privacy & Security
+        bitwarden
+
+        # Entertainment
+        spotify
+        steam
+
+        # System
+        syncthing
+        syncthing-tray
+
+        # Audio / video
+        vlc 
+      ];
+
+      programs = {
+        git =  {
+          enable = true;
+          lfs.enable = true;
+
+          userName = "Jonathyn Stiverson";
+          userEmail = "jlstiverson2002@protonmail.com";
+
+          aliases = {
+              gc = "commit";
+              gp = "push";
+              ga = "add";
+          };
+        }
+      };
+
+    }
+  };
 }
