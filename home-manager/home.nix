4ed6a0c92ca2d3b9524059f8b3517f8216d37b22
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2024-03-22 22:11:27 -0500

    Update home.nix module

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,14 +1,18 @@
 # This is your home-manager configuration file
 # Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
-{ inputs, lib, config, pkgs, ... }:
-let 
-  home-manager.url = "github:nix-community/home-manager/release-23.05";
-  home-manager.inputs.nixpkgs.follows = "nixpkgs";
-in
+{ flake, pkgs, ... }:
 {
   imports = [
     (import "${home-manager}/nixos")
   ];
+
+  nix = {
+    registry.nixpkgs.flake = flake.inputs.nixpkgs;
+
+    gc = {
+      automatic = true;
+    }
+  };
 
   nixpkgs = {
     overlays = [];
