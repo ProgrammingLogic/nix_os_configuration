7b011017ee2c23d4a63aaed890a867cc541e1daf
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2024-03-22 20:58:37 -0500

    Add legacy inputs to system's legacy channels

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -18,6 +18,18 @@
   # To make nix3 commands consistent with your flake
   nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
 
+  # This will additionally add your inputs to the system's legacy channels
+  # Making legacy nix commands consistent as well, awesome!
+  nix.nixPath = ["/etc/nix/path"];
+  environment.etc =
+    lib.mapAttrs'
+    (name: value: {
+      name = "nix/path/${name}";
+      value.source = value.flake;
+    })
+    config.nix.registry;
+
+
   nix.settings = {
     experimental-features = "nix-command flakes";
 
