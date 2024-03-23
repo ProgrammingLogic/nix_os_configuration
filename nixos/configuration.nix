eb6dcb7978c0dba62f119d57b06f99db163005e0
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2024-03-23 13:15:48 -0500

    Add public key for jstiverson-desktop

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -145,6 +145,7 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDyA94BA6rCIb8Zz2/3EbvdYIgG9gS98baoKA3E0s1a5 jstiverson@nixos"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPlXe5keHP0Nj8DWxcppx/3GKn+4GzNarQ44cxKr4eZD jstiverson@nixos"
+      "sh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFJb0myLAr6C7AEuxOZ7G+/zeN4SjRJ09EwgD0C7x7J/ jstiverson@jstiverson-desktop"
     ];
   };
 }
